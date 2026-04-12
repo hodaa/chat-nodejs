c1ad984604263adfb3776fac078d9370053b65c7
hodaa <hoda.hussin@gmail.comm>
2026-04-12 09:35:29 +0200

    first push

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import {createServer} from 'http'
+import {WebSocket, WebSocketServer } from "ws";
+import redis, { Redis } from 'ioredis'
+
+
+const server = createServer();
+const wss = new WebSocketServer({ server });
+
+const sub = new redis();
+const pub = new redis();
+
+
+
+sub.subscribe("chat_message")
+sub.on('message', (channel, msg) => {
+    for (const client of wss.clients) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(msg)
+        }
+    }
+})
+
+wss.on('connection', client => {
+    //When a new message is received from a connected client,
+    client.on('message', msg => {
+        const message = msg.toString()
+        console.log(message)
+        
+        pub.publish('chat_message', message)
+    })
+})
+
+
+
+// const redisClient = new Redis();
+// const redisClientXRead = new redis();
+
+// wss.on('connection', client => {
+//     //When a new message is received from a connected client,
+//     client.on('message', msg => {
+//         const message = msg.toString()
+//         console.log(message )
+//         redisClient.xadd('chat_message', message)
+//     })
+// })
+
+
+server.listen(process.argv[2] || 8000)
